dcdbe22d59165c9d1df6da8f2a9c52ddc60b155e
Benjamin Derksen <b.derksen@mail.de>
2021-06-29 17:05:22 +0200

    adding routes

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import "./App.css";
 import { useState } from "react";
 import Todo from "./Screens/Todo";
 import Navbar from "./Components/Navbar";
+import { Switch, Route } from "react-router-dom";
 
 function App() {
   const [searchQuery, setSearchQuery] = useState("");
@@ -11,7 +12,14 @@ function App() {
       <Navbar onSearchQuery={(e) => setSearchQuery(e.target.value)} />
 
       <div className="MainContainer">
-        <Todo searchQuery={searchQuery} />
+        <Switch>
+          <Route exact path="/">
+            <Todo searchQuery={searchQuery} />
+          </Route>
+          <Route path="*">
+            <h1>404 - not found</h1>
+          </Route>
+        </Switch>
       </div>
     </div>
   );
