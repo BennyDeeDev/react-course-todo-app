d40f5087279d7da4699645c9f52121e11398692d
Benjamin Derksen <b.derksen@mail.de>
2021-07-03 16:44:03 +0200

    app state in components

--- src/Screens/Todo.jsx
+++ src/Screens/Todo.jsx
@@ -1,50 +1,43 @@
 import React from "react";
+import { useSelector } from "react-redux";
 import TodoList from "../Todo/TodoList";
+import { v4 as uuid } from "uuid";
+import { useDispatch } from "react-redux";
+import { addTodo } from "../Reducer/todoSlice";
 
-const Todo = ({
-  todos,
-  onAddTodo,
-  onDeleteTodo,
-  onToggleTodo,
-  searchQuery,
-}) => {
-  const activeTodos = todos.filter(({ done }) => !done);
-  const filteredTodos = todos.filter((t) =>
+const Todo = ({ searchQuery }) => {
+  const todoList = useSelector((state) => state.todo.list);
+  const dispatch = useDispatch();
+  const activeTodos = todoList.filter(({ done }) => !done);
+  const filteredTodos = todoList.filter((t) =>
     t.title.toLowerCase().includes(searchQuery.toLowerCase())
   );
-  const doneTodos = todos.filter(({ done }) => done);
+  const doneTodos = todoList.filter(({ done }) => done);
+
+  const handleAddTodo = (event) => {
+    if (event.key === "Enter") {
+      dispatch(addTodo({ id: uuid(), title: event.target.value, done: false }));
+
+      event.target.value = "";
+    }
+  };
 
   return (
     <>
       <div className="TodoInputContainer">
         <input
-          onKeyDown={onAddTodo}
+          onKeyDown={handleAddTodo}
           placeholder="Hier Todo hinzufügen"
           className="InputItem"
         />
       </div>
 
       {searchQuery ? (
-        <TodoList
-          title="Suchergebnisse:"
-          list={filteredTodos}
-          onDeleteTodo={onDeleteTodo}
-          onToggleTodo={onToggleTodo}
-        />
+        <TodoList title="Suchergebnisse:" list={filteredTodos} />
       ) : (
         <>
-          <TodoList
-            title="Zu erledigen"
-            list={activeTodos}
-            onDeleteTodo={onDeleteTodo}
-            onToggleTodo={onToggleTodo}
-          />
-          <TodoList
-            title="Erledigt"
-            list={doneTodos}
-            onDeleteTodo={onDeleteTodo}
-            onToggleTodo={onToggleTodo}
-          />
+          <TodoList title="Zu erledigen" list={activeTodos} />
+          <TodoList title="Erledigt" list={doneTodos} />
         </>
       )}
     </>
--- src/Screens/TodoDetails.jsx
+++ src/Screens/TodoDetails.jsx
@@ -1,16 +1,20 @@
 import React, { useState } from "react";
 import { useHistory, useParams } from "react-router-dom";
+import { useSelector, useDispatch } from "react-redux";
+import { changeTodoTitle } from "../Reducer/todoSlice";
 
-const TodoDetails = ({ todos, onChangeTodo }) => {
+const TodoDetails = () => {
+  const todoList = useSelector((state) => state.todo.list);
+  const dispatch = useDispatch();
   const { id } = useParams();
   const history = useHistory();
-  const todo = todos.find((t) => t.id === id);
+  const todo = todoList.find((t) => t.id === id);
   const [title, setTitle] = useState(todo.title);
 
   const handleChangeTitle = (e) => {
     console.log(e);
     if (e.key === "Enter") {
-      onChangeTodo({ ...todo, title });
+      dispatch(changeTodoTitle({ ...todo, title }));
       history.push("/");
     }
   };
